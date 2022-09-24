482971cbeefb2baf9f64bf1185a2485c6495abfc
Geisa Cruz <eugeisacruz@gmail.com>
2022-09-24 17:17:55 -0300

    listar categoria

--- src/components/Filtro/Filtro.jsx
+++ src/components/Filtro/Filtro.jsx
@@ -30,9 +30,37 @@ class Filtro extends React.Component {
      }
       
       render() {
+        const itens = (categoria) => {
+          return categoria.itens.map((item) => (
+                <div className="itens">
+                <img>{item.imagem}</img>
+                <div className="texto">
+                    <p id="nome">{item.nome}</p>
+                    <p1>{item.descricao}</p1>
+                    <p id="preco">{item.preco}</p>
+                </div>
+                </div>
+            ));
+        };
+
+        const categorias = (cardapio) => {
+          return cardapio.map((categoria) => (
+                <div>
+                  <h1>{categoria.categoria}</h1><br/>
+                </div> 
+            ));
+        };
+
+
         return (
             <div className="root">
-                <Input onChange={text => this.filtrar(text)} />
+              <div class="filtro">
+                  <Input onChange={text => this.filtrar(text)} />
+                </div>
+
+                <br/>
+                <br/>
+                {categorias(this.state.restauranteFiltrado.cardapio)}
             </div>
         );
       }
