6fc821ff5938c4e5784bdab4c8d90b6a2670b6c0
bruholanda <brunaholandacunha7@gmail.com>
2022-09-24 14:55:05 -0300

    fix: import useParams deletado. linha de import deletada, pois useParams já está sendo importado

--- src/pages/Restaurantes/RestaurantesPage.jsx
+++ src/pages/Restaurantes/RestaurantesPage.jsx
@@ -2,7 +2,6 @@ import { Container, Grid, Typography, CircularProgress } from "@material-ui/core
 import { useEffect, useState } from "react";
 import { getRestaurantes } from "../../services/restaurantes.service";
 import { useNavigate, useParams } from "react-router-dom";
-// import { useParams } from "react-router-dom"; 
 import "./style.css";
 
 function RestaurantesPage() {
